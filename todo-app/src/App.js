6d894dd0319d94d8e463b440dc2c6dd1c3d818fb
mhpark <minoflower31@naver.com>
2024-12-02 23:50:17 +0900

    splash, icon 추가. splash 미적용

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,16 +1,16 @@
 import styled from "styled-components/native";
-import {ThemeProvider} from "styled-components";
-import {theme} from "./theme";
-import React, {useState} from "react";
-import {StatusBar, useWindowDimensions} from "react-native";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme";
+import React, { useCallback, useEffect, useState } from "react";
+import { StatusBar, useWindowDimensions } from "react-native";
 import Input from "./components/input";
-import IconButton from "./components/IconButton";
-import {icons} from "./icons";
 import Task from "./components/Task";
+import * as SplashScreen from "expo-splash-screen";
+import AsyncStorageNative from "@react-native-async-storage/async-storage/src/AsyncStorage.native";
 
 const Container = styled.SafeAreaView`
   flex: 1;
-  background-color: ${({theme}) => theme.background};
+  background-color: ${({ theme }) => theme.background};
   align-items: center;
   justify-content: flex-start;
 `;
@@ -18,83 +18,133 @@ const Container = styled.SafeAreaView`
 const Title = styled.Text`
   font-size: 40px;
   font-weight: 600;
-  color: ${({theme}) => theme.main};
+  color: ${({ theme }) => theme.main};
   width: 100%;
   align-items: flex-end;
   padding: 0 20px;
-`
+`;
 
 const List = styled.ScrollView`
   flex: 1;
-  width: ${({width}) => width - 40}px;
+  width: ${({ width }) => width - 40}px;
 `;
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   const width = useWindowDimensions().width;
   const [newTask, setNewTask] = useState("");
 
-  const tempData = {
-    1: {id: "1", text: "React", completed: false}
-  }
+  const [tasks, setTasks] = useState({});
 
-  const [tasks, setTasks] = useState(tempData);
+  const storeData = async (tasks) => {
+    try {
+      await AsyncStorageNative.setItem("tasks", JSON.stringify(tasks));
+      setTasks(tasks);
+    } catch (e) {}
+  };
+
+  const getData = async () => {
+    try {
+      const loadedData = await AsyncStorageNative.getItem("tasks");
+      setTasks(JSON.parse(loadedData || "{}"));
+    } catch (e) {}
+  };
 
   const addTask = () => {
-    if(newTask.length <= 0) {
+    if (newTask.length <= 0) {
       alert("할 일을 입력해 주세요.");
       return;
     }
 
     const ID = Date.now().toString();
     const newTaskObj = {
-      [ID]: {id: ID, text: newTask, completed: false}
-    }
+      [ID]: { id: ID, text: newTask, completed: false },
+    };
 
-    setTasks({...tasks, ...newTaskObj});
+    storeData({ ...tasks, ...newTaskObj });
     setNewTask("");
     alert("Write");
-  }
+  };
 
   const deleteTask = (id) => {
     const currentTasks = Object.assign({}, tasks);
     delete currentTasks[id];
 
     // delete tasks[id];
-    setTasks(currentTasks);
-  }
+    storeData(currentTasks);
+  };
 
   const toggleTask = (id) => {
     const currentTasks = Object.assign({}, tasks);
-    currentTasks[id]['completed'] = !currentTasks[id]['completed'];
-    setTasks(currentTasks);
-  }
+    currentTasks[id]["completed"] = !currentTasks[id]["completed"];
+    storeData(currentTasks);
+  };
 
-  const updateTask = item => {
+  const updateTask = (item) => {
     const currentTasks = Object.assign({}, tasks);
     currentTasks[item.id] = item;
-    setTasks(currentTasks);
+    storeData(currentTasks);
+  };
+
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    const prepare = async () => {
+      try {
+        getData();
+        await new Promise((resolve) => {
+          setTimeout(resolve, 2000);
+        });
+      } catch (error) {
+        console.warn(error);
+      } finally {
+        setIsReady(true);
+      }
+    };
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(() => {
+    if (isReady) {
+      SplashScreen.hide();
+    }
+  }, [isReady]);
+
+  if (!isReady) {
+    return null;
   }
 
   return (
-      <ThemeProvider theme={theme}>
-        <Container>
-          <StatusBar barStyle={"light-content"}
-                     backgroundColor={theme.background}/>
-          <Title>TODO List</Title>
-          <Input placeholder="Add a task" value={newTask}
-                 onChangeText={text => setNewTask(text)}
-                 onSubmitEditing={addTask}
-                 onBlur={() => setNewTask("")}
-          />
-          <List width={width}>
-            {Object.values(tasks)
+    <ThemeProvider theme={theme}>
+      <Container onLayout={onLayoutRootView}>
+        <StatusBar
+          barStyle={"light-content"}
+          backgroundColor={theme.background}
+        />
+        <Title>TODO List</Title>
+        <Input
+          placeholder="Add a task"
+          value={newTask}
+          onChangeText={(text) => setNewTask(text)}
+          onSubmitEditing={addTask}
+          onBlur={() => setNewTask("")}
+        />
+        <List width={width}>
+          {Object.values(tasks)
             .reverse()
-            .map((task) => <Task key={task.id} task={task}
-                                 onDeleteTask={deleteTask}
-                                 onToggleTask={toggleTask}
-                                 onUpdateTask={updateTask}/>)}
-          </List>
-        </Container>
-      </ThemeProvider>
+            .map((task) => (
+              <Task
+                key={task.id}
+                task={task}
+                onDeleteTask={deleteTask}
+                onToggleTask={toggleTask}
+                onUpdateTask={updateTask}
+              />
+            ))}
+        </List>
+      </Container>
+    </ThemeProvider>
   );
 }
